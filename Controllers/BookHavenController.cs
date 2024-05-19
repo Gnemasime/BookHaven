815c639634d31c8d191a16044b9e46784aae8769
Gnemasime Github <smesihlentshangase@gmail.com>
2024-05-19 19:57:54 +0200

    Add project files.

---
+++ Controllers/BookHavenController.cs
@@ -0,0 +1,29 @@
+﻿using BookHaven.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BookHaven.Controllers
+{
+    public class BookHavenController : Controller
+    {
+        // GET: BookHaven
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Index(BookOrder bo)
+        {
+            bo.BasicCost = bo.CalcBasicTotal();
+            bo.ExtraCost = bo.CalcExtra();
+            bo.TotalCostSingle = bo.CalcTotalCostSingle();
+            bo.TotalCost = bo.CalcTotalCost();
+            bo.VatDue = bo.CalcVatDue();
+            bo.FinalCost = bo.CalcFinalCost();
+            return View(bo);
+        }
+    }
+}
---
+++ Models/BookOrder.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BookHaven.Models
+{
+
+    public class BookOrder
+    {
+        public string CustomerName { get; set; }
+
+        public string BookType { get; set; }
+
+        public bool GiftWrap { get; set; }
+
+        public bool ExpressDelivery { get; set; }
+
+        public int Qty { get; set; }
+
+        public double BasicCost { get; set; }
+
+        public double ExtraCost { get; set; }
+
+        public double TotalCostSingle { get; set; }
+
+        public double TotalCost { get; set; }
+
+        public double VatDue { get; set; }
+
+        public double FinalCost { get; set; }
+
+        public double CalcBasicTotal()
+        {
+            double basic = 0.00;
+            if (BookType == "Fiction")
+            {
+                basic = 150;
+            }
+            else if (BookType == "Non-Fiction")
+            {
+                basic = 200;
+            }
+            else if (BookType == "Comics")
+            {
+                basic = 100;
+            }
+            return basic;
+        }
+
+        public double CalcExtra()
+        {
+            double extra = 0.00;
+            if (GiftWrap == true)
+            {
+                extra += 50;
+            }
+            if (ExpressDelivery == true)
+            {
+                extra += 30;
+            }
+            return extra;
+        }
+
+        public double CalcTotalCostSingle()
+        {
+            return CalcBasicTotal() * Qty;
+        }
+
+        public double CalcTotalCost()
+        {
+            return CalcTotalCostSingle() + CalcExtra();
+        }
+
+        public double CalcVatDue()
+        {
+            return CalcTotalCost() * 0.15;
+        }
+
+        public double CalcFinalCost()
+        {
+            return CalcTotalCost() + CalcVatDue();
+        }
+    }
+
+}
